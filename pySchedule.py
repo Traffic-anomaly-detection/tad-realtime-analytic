38f59ac08be2890cd65a094851d9c6d0f228d07a
Nitikorn-T <ichromex@hotmail.com>
2022-02-04 12:16:48 +0700

    add new field to database

--- pySchedule.py
+++ pySchedule.py
@@ -1,4 +1,3 @@
-import firebase_admin
 import schedule
 import time
 import pandas as pd
@@ -17,6 +16,7 @@ TAD_API = os.getenv('TAD_API')
 
 df_km127 = pd.read_csv("./dataset/latlon_km127.csv")
 
+
 def csv_file():
     header_list = ['datetime', 'road_number', 'km', 'direction', 'all_units', 'inflow_units',
                    'outflow_unit', 'samecell_units', 'avg_speed', 'max_speed', 'avg_traveltime', 'max_traveltime']
@@ -31,13 +31,16 @@ def csv_file():
     accdate = str(df_traffic_wlatlon.loc[rand_idx, 'datetime'])
     accroad_no = str(df_traffic_wlatlon.loc[rand_idx, 'road_number'])
     acckm = str(df_traffic_wlatlon.loc[rand_idx, 'km'])
+    accdirection = str(df_traffic_wlatlon.loc[rand_idx, 'direction'])
     acclat = str(df_traffic_wlatlon.loc[rand_idx, 'lat'])
     acclon = str(df_traffic_wlatlon.loc[rand_idx, 'lon'])
-    post_db(accdate, acclat, acclon)
-    line_notify(accroad_no, acckm, acclat, acclon, accdate)
+    post_db(accroad_no, acckm, accdirection, acclat, acclon, accdate)
+    line_notify(accroad_no, acckm, accdirection, acclat, acclon, accdate)
+
 
 def filter_traffic(df):
     return df[(df['road_number'] == 1) | (df['road_number'] == 2) | (df['road_number'] == 7)]
+
 
 def map_traffic_with_latlon(df):
     df['lat'] = df.apply(lambda row: df_km127[(df_km127['rd'] == row['road_number']) & (df_km127['km'] == row['km'])]['lat'].values[0]
@@ -47,7 +50,7 @@ def map_traffic_with_latlon(df):
     return df
 
 
-def line_notify(road, km, lat, lon, date):
+def line_notify(road, km, direction, lat, lon, date):
     headers = {
         'content-type':
             'application/x-www-form-urlencoded',
@@ -55,14 +58,14 @@ def line_notify(road, km, lat, lon, date):
     }
     # msg = input("Enter your name:")
     # msg = date + " Accident at " + "Latitude : " + lat + ", Longitude : " + lon
-    msg = f'{date} Accident at \nRoad number : {road} \nKm : {km} \nLatitude : {lat} \nLongitude : {lon}'
+    msg = f'{date} Accident at \nRoad number : {road} \nKm : {km} \nDirection : {direction} \nLatitude : {lat} \nLongitude : {lon}'
     r = requests.post(LINE_URL, headers=headers, data={'message': msg})
     print(r.text)
 
 
-def post_db(road, km, lat, lon, date):
+def post_db(road, km, direction, lat, lon, date):
     res = requests.post(TAD_API, data={
-                        "road_no": road, "km": km, "lat": lat, "lon": lon, "date_time": date})
+                        "road_no": road, "km": km, "direction": direction, "lat": lat, "lon": lon, "date_time": date})
     print(res.text)
     # ref = db.reference('Accident')
     # data = {
